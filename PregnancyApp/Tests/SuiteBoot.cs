fb793040677174fa6eca0922228f16fe126d1e21
DerYak1999 <yakirlevy1290@gmail.com>
2025-12-11 19:04:53 +0200

    Move boot test to Tests/SuiteBoot.cs and remove AppLaunching.cs from Tests; setup lives in Helpers

--- PregnancyApp/Tests/SuiteBoot.cs
+++ PregnancyApp/Tests/SuiteBoot.cs
@@ -1,9 +1,7 @@
 using NUnit.Framework;
-using PregnancyApp.Helpers;
 
 namespace PregnancyApp.Tests
 {
-    // Minimal test to trigger discovery/run; real tests can be added here
     public class SuiteBoot
     {
         [Test]
